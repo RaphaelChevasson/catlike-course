2231dcbfd940b05ef34b0617844bacb31b7e6ac9
RaphaelChevasson <rc@clubcapelli.com>
2024-10-11 18:37:45 +0200

    Update pseudorandom surfaces - perlin derivatives

--- Catlike 05 - Pseudorandom Surfaces/Assets/Scripts/Surfaces/ProceduralSurface.cs
+++ Catlike 05 - Pseudorandom Surfaces/Assets/Scripts/Surfaces/ProceduralSurface.cs
@@ -35,6 +35,24 @@ public class ProceduralSurface : MonoBehaviour {
 
 	static SurfaceJobScheduleDelegate[,] surfaceJobs = {
 		{
+			SurfaceJob<Lattice1D<LatticeNormal, Perlin>>.ScheduleParallel,
+			SurfaceJob<Lattice2D<LatticeNormal, Perlin>>.ScheduleParallel,
+			SurfaceJob<Lattice3D<LatticeNormal, Perlin>>.ScheduleParallel
+		},
+		{
+			SurfaceJob<Lattice1D<LatticeNormal, Smoothstep<Turbulence<Perlin>>>>
+				.ScheduleParallel,
+			SurfaceJob<Lattice2D<LatticeNormal, Smoothstep<Turbulence<Perlin>>>>
+				.ScheduleParallel,
+			SurfaceJob<Lattice3D<LatticeNormal, Smoothstep<Turbulence<Perlin>>>>
+				.ScheduleParallel
+		},
+		{
+			SurfaceJob<Lattice1D<LatticeNormal, Value>>.ScheduleParallel,
+			SurfaceJob<Lattice2D<LatticeNormal, Value>>.ScheduleParallel,
+			SurfaceJob<Lattice3D<LatticeNormal, Value>>.ScheduleParallel
+		},
+		{
 			SurfaceJob<Simplex1D<Simplex>>.ScheduleParallel,
 			SurfaceJob<Simplex2D<Simplex>>.ScheduleParallel,
 			SurfaceJob<Simplex3D<Simplex>>.ScheduleParallel
@@ -52,6 +70,7 @@ public class ProceduralSurface : MonoBehaviour {
 	};
 
 	public enum NoiseType {
+		Perlin, PerlinSmoothTurbulence, PerlinValue,
 		Simplex, SimplexSmoothTurbulence, SimplexValue
 	}
 
